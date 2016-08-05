6cb3a0ff0e5c3f042a2aa1910e50a49273ec8831
Matthew McLaughlin <Matthew McLaughlin>
2016-08-05 05:37:51 -0700

    basic data in with string formatting, unstyled

--- react-petlist/src/components/Petlist.js
+++ react-petlist/src/components/Petlist.js
@@ -4,10 +4,13 @@ const Petlist = ({ vacayData }) => {
   let petlist;
   if (vacayData.length) {
     petlist = vacayData.map(element => {
-      const name = formatName(element.user.first, element.user.last);
+      // handle inconsistent description key naming
+      const descriptionString = element.description || element.desciption;
       return (<div className="pet-info-block">
-        <h2>{name}</h2>
-        <h1>{element.title}</h1>
+        <h1 id="user-name">{formatName(element.user.first, element.user.last)}</h1>
+        <p id="title">{formatTitle(element.title)}</p>
+        <p id="description">{formatDescription(descriptionString, 48)}</p>
+        <p id="pet-name">{element.pet.name}</p>
       </div>
       );
     });
@@ -21,10 +24,23 @@ const Petlist = ({ vacayData }) => {
 };
 
 const formatName = (first, last) => {
-  const formattedFirst = first.charAt(0).toUpperCase() + first.substr(1);
-  const formattedLast = `${last.charAt(0).toUpperCase()}.`;
+  const formattedFirst = first.trim().charAt(0).toUpperCase() + first.substr(1);
+  const formattedLast = `${last.trim().charAt(0).toUpperCase()}.`;
   return `${formattedFirst} ${formattedLast}`;
-}
+};
+
+const formatTitle = title => {
+  return title.trim().replace(/  +/g, ' ');
+};
+
+const formatDescription = (str, cutoff) => {
+  str = str.trim().replace(/  +/g, ' ');
+  if (str.length < cutoff) return str;
+  if (str.charAt(cutoff - 1) !== ' ' && str.charAt(cutoff) !== ' ') {
+    return `${str.substr(0, str.lastIndexOf(' '))}...`;
+  }
+  return `${str.substr(0, cutoff).trim()}...`;
+};
 
 Petlist.propTypes = {
   vacayData: React.PropTypes.array.isRequired,
--- react-petlist/src/components/Search.js
+++ react-petlist/src/components/Search.js
@@ -3,6 +3,8 @@ const React = require('react');
 const Search = () => {
   return (
     <div className="search-bar">
+      <div id="boarding">Boarding</div>
+      <div id="sitting">Sitting</div>
     </div>
   );
 };
